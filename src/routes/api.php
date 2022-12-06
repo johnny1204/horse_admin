6f8daf5c18f79aec87fb7069e820b8f0fb72c671
johnny1204 <major@za2.so-net.ne.jp>
2022-12-06 22:19:00 +0900

    chore: add redis

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function () {
+    Route::get('places', 'Race\PlacesController');
+    Route::get('pillars', 'Race\PillarController');
+    Route::get('jockey_rate', 'Race\JockeyRateConrtoller');
+});
